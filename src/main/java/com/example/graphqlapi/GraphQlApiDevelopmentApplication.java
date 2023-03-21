fedaca527361743ce788f35379fd3a67c87190bf
Aris Tan <arisromil@yahoo.com>
2023-03-21 12:05:58 -0600

    GraphQL API Development

---
+++ src/main/java/com/example/graphqlapi/GraphQlApiDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphqlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlApiDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlApiDevelopmentApplication.class, args);
+	}
+
+}
